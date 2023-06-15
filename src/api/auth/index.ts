096c85b1992df189e77cda10e275ead04b7b3cd7
Arul Valan Anto S <61511921+arulvalananto@users.noreply.github.com>
2023-06-16 01:55:02 +0530

    refactor: API section

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,59 +1,82 @@
-/* eslint-disable no-async-promise-executor */
 import { AxiosError } from 'axios';
 
 import { checkTokenExpiry } from '../helpers';
 import axiosInstance, { headerConfig } from '..';
 import constants from '../../static/constants.json';
 
-/**
- * Generate Access Token
- */
-export const getAuthToken = (): Promise<string> => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const response = await axiosInstance.get(
-                `/auth/trial/accounts/${constants.AZURE_VIDEO_INDEXER.ACCOUNT_ID}/AccessToken?allowEdit=true`,
-                headerConfig
-            );
-            if (response?.status === 200 && response?.data) {
-                localStorage.setItem(constants.TOKEN_NAME, response?.data);
-                const token: string = response?.data;
-                resolve(token);
-            } else {
-                throw new Error(constants.ERROR_MESSAGE.SOMETHING_WENT_WRONG);
-            }
-        } catch (error: unknown) {
-            if (error instanceof Error) reject(error?.message);
-            else if (error instanceof AxiosError)
-                reject(error?.response?.data?.message);
-        }
-    });
-};
+class AuthAPI {
+    accountId: string;
+    urlPrefix: string;
 
-/**
- * Get access token for video streaming service
- * @param videoId
- * @returns
- */
-export const getVideoAccessToken = (videoId: string): Promise<string> => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            await checkTokenExpiry();
+    constructor() {
+        this.accountId = constants.AZURE_VIDEO_INDEXER.ACCOUNT_ID;
+        this.urlPrefix = `/auth/trial/accounts/${this.accountId}`;
+    }
 
-            const response = await axiosInstance.get(
-                `/Auth/trial/Accounts/${constants.AZURE_VIDEO_INDEXER.ACCOUNT_ID}/Videos/${videoId}/AccessToken?allowEdit=true`,
-                headerConfig
-            );
+    /**
+     * Generate Access Token
+     */
+    getAuthToken(): Promise<string> {
+        return new Promise((resolve, reject) => {
+            axiosInstance
+                .get(
+                    `${this.urlPrefix}/AccessToken?allowEdit=true`,
+                    headerConfig
+                )
+                .then((response) => {
+                    if (response?.status === 200 && response?.data) {
+                        localStorage.setItem(
+                            constants.TOKEN_NAME,
+                            response?.data
+                        );
+                        const token: string = response?.data;
+                        resolve(token);
+                    } else {
+                        throw new Error(
+                            constants.ERROR_MESSAGE.SOMETHING_WENT_WRONG
+                        );
+                    }
+                })
+                .catch((error: unknown) => {
+                    if (error instanceof Error) reject(error?.message);
+                    else if (error instanceof AxiosError)
+                        reject(error?.response?.data?.message);
+                });
+        });
+    }
 
-            if (response?.data) {
-                resolve(response?.data);
-            } else {
-                reject(constants.ERROR_MESSAGE.SOMETHING_WENT_WRONG);
-            }
-        } catch (error: unknown) {
-            if (error instanceof Error) reject(error?.message);
-            else if (error instanceof AxiosError)
-                reject(error?.response?.data?.message);
-        }
-    });
-};
+    /**
+     * Get access token for video streaming service
+     * @param videoId
+     * @returns
+     */
+    getVideoAccessToken(videoId: string): Promise<string> {
+        return new Promise((resolve, reject) => {
+            checkTokenExpiry()
+                .then(() => {
+                    axiosInstance
+                        .get(
+                            `${this.urlPrefix}/Videos/${videoId}/AccessToken?allowEdit=true`,
+                            headerConfig
+                        )
+                        .then((response) => {
+                            if (response?.data) {
+                                resolve(response?.data);
+                            } else {
+                                reject(
+                                    constants.ERROR_MESSAGE.SOMETHING_WENT_WRONG
+                                );
+                            }
+                        });
+                })
+                .catch((error: unknown) => {
+                    if (error instanceof Error) reject(error?.message);
+                    else if (error instanceof AxiosError)
+                        reject(error?.response?.data?.message);
+                });
+        });
+    }
+}
+
+const AuthAPIInstance = new AuthAPI();
+export default AuthAPIInstance;
